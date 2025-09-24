1e1e0a8ce41591abdbcf8150a8da74250d9d35c5
beucismis <beucismis@tutamail.com>
2025-09-24 09:21:57 +0300

    Update configs.py

--- src/koyunkapan/bot/configs.py
+++ src/koyunkapan/bot/configs.py
@@ -4,7 +4,6 @@ import os
 DATA_DIR = os.environ.get("KOYUNKAPAN_DATA_DIR")
 LOG_FILE = os.path.join(DATA_DIR, "app.log")
 DB_FILE = os.path.join(DATA_DIR, "app.db")
-PRAW_FILE = os.path.join(DATA_DIR, "praw.ini")
 
 POST_LIMIT = 50
 SEARCH_LIMIT = 20
@@ -15,15 +14,17 @@ SIMILARITY_THRESHOLD = 1.35
 
 SUBREDDIT_NAMES = [
     "test",
-    "amip",
+    "KGBTR",
     "sinema",
     "bokteri",
-    "vlandiya",
     "Eleteria",
     "CuteTopia",
     "WeebTurks",
     "Cikopolis",
+    "TurkishCats",
+    "Turkishdogs",
     "SacmaBirSub",
+    "kopyamakarna",
     "CursedYemekler",
 ]
 
