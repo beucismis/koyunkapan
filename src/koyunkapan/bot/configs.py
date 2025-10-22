762d16be34e0827a082a2979b046d45d806ccb06
beucismis <beucismis@tutamail.com>
2025-10-22 20:22:41 +0300

    Update configs.py

--- src/koyunkapan/bot/configs.py
+++ src/koyunkapan/bot/configs.py
@@ -5,8 +5,8 @@ DATA_DIR = os.environ.get("KOYUNKAPAN_DATA_DIR")
 LOG_FILE = os.path.join(DATA_DIR, "app.log")
 DB_FILE = os.path.join(DATA_DIR, "app.db")
 
-MIN_SLEEP_MINUTES = 5
-MAX_SLEEP_MINUTES = 20
+MIN_SLEEP_MINUTES = 10
+MAX_SLEEP_MINUTES = 25
 INBOX_CHECK_INTERVAL = 60
 WORKING_HOURS = [str(i).zfill(2) for i in range(24)]
 
