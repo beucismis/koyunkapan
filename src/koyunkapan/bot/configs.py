67afcfabd23fff94b142295c736fe98bde8d5242
beucismis <beucismis@tutamail.com>
2026-01-29 21:00:01 +0300

    Update configs.py

--- src/koyunkapan/bot/configs.py
+++ src/koyunkapan/bot/configs.py
@@ -15,20 +15,33 @@ MAX_KEYWORDS = 25
 RANDOM_POST_COUNT = 10
 TOP_COMMENT_LIMIT = 10
 SIMILARITY_THRESHOLD = 1.35
+MIN_SUBMISSION_THRESHOLD = 20
+TIER_2_SUBREDDIT_COUNT = 5
 
 SUBREDDIT_WEIGHTS = {
-    "KGBTR": 2.0,
+    "amip": 1.0,
+    "KGBTR": 1.5,
+    "delik": 1.0,
+    "dewrim": 1.0,
     "bokteri": 1.0,
     "Eleteria": 1.0,
+    "pedlesme": 1.0,
     "CuteTopia": 1.0,
     "WeebTurks": 1.0,
-    "Cikopolis": 1.1,
+    "Cikopolis": 1.0,
+    "rockmuzik": 1.0,
+    "StresOdasi": 1.0,
     "TurkishCats": 1.0,
     "Turkishdogs": 1.0,
-    "SacmaBirSub": 1.1,
+    "SacmaBirSub": 1.0,
     "kopyamakarna": 1.0,
-    "BAYIRDOMUZLARI": 1.2,
+    "aptalSoruYok": 1.0,
+    "TurkeyJerky": 1.0,
+    "YIKIKHAYATLAR": 1.0,
+    "yalnizucubeler": 1.0,
+    "BAYIRDOMUZLARI": 1.0,
     "CursedYemekler": 1.0,
+    "MutfakDertlileri": 1.0,
 }
 
 FORBIDDEN_FLAIR = "Ciddi"
